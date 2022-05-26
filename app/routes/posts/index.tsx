8a22555f1c3a853e255511433b09a02e4cb529bf
Andres Fernandez <andres.fernandez@truepill.om>
2022-05-26 01:06:32 -0500

    feat: create posts loader

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,4 +1,23 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  return json({
+    posts: [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A Mixtape I Made Just For You",
+      },
+    ],
+  });
+};
 export default function Posts() {
+  const { posts } = useLoaderData();
+  console.log("🚀 ~ file: index.tsx ~ line 20 ~ Posts ~ posts", posts);
   return (
     <main>
       <h1>Posts</h1>
