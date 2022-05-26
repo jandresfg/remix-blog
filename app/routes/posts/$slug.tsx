cce118aa5965ccdf1a172b8ae18b91c62e3a724d
Andres Fernandez <andres.fernandez@truepill.om>
2022-05-26 17:03:38 -0500

    feat: assert existing slug and corresponding post using invariant

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -2,9 +2,12 @@ import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/node";
 import { getPost } from "~/models/post.server";
+import invariant from "tiny-invariant";
 
 export const loader: LoaderFunction = async ({ params }) => {
+  invariant(params.slug, "params.slug is required");
   const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
   return json({ post });
 };
 
