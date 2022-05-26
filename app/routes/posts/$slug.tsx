443e50f76aac3eb9a643dfdeddd52d263eb8909e
Andres Fernandez <andres.fernandez@truepill.om>
2022-05-26 16:29:11 -0500

    feat: create dynamic $slug route

---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,7 @@
+export default function PostSlug() {
+  return (
+    <main className="mx-auto max-w-4xl">
+      <h1 className="my-6 border-b-2 text-center text-3xl">some post</h1>
+    </main>
+  );
+}
