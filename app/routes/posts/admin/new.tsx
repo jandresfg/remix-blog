650e3cd334111d3d2b59abb53674bef76dae19f5
Andres Fernandez <andres.fernandez@truepill.om>
2022-05-27 01:33:42 -0500

    feat: useTransition to disable submit and show "creating..."

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,4 +1,4 @@
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useTransition } from "@remix-run/react";
 import { createPost } from "~/models/post.server";
 import { redirect, json } from "@remix-run/node";
 import type { ActionFunction } from "@remix-run/node";
@@ -12,6 +12,9 @@ type ActionData =
   | undefined;
 
 export const action: ActionFunction = async ({ request }) => {
+  // TODO: remove me
+  await new Promise((res) => setTimeout(res, 1000));
+
   const formData = await request.formData();
   const title = formData.get("title");
   const slug = formData.get("slug");
@@ -40,6 +43,9 @@ export const action: ActionFunction = async ({ request }) => {
 
 export default function NewPost() {
   const errors = useActionData();
+  const transition = useTransition();
+  const isCreating = Boolean(transition.submission);
+
   return (
     <Form method="post">
       <p>
@@ -72,9 +78,10 @@ export default function NewPost() {
       <p className="text-right">
         <button
           type="submit"
+          disabled={isCreating}
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
         >
-          Create Post
+          {isCreating ? "Creating..." : "Create Post"}
         </button>
       </p>
     </Form>
